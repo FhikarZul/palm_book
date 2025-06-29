998b22a7ec35e44b1b1e521c4717a3f99aea0f6a
Fhikar Zul <Fhikarzul18@gmail.com>
2025-06-30 00:02:22 +0800

    feat: update http client (cached)

---
+++ lib/core/utils/http_client.dart
@@ -0,0 +1,54 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+class HttpClient {
+  final Dio _dio = Dio();
+  final SharedPreferences _prefs;
+
+  HttpClient(this._prefs);
+
+  Future<Response> get(String url, {Map<String, dynamic>? query}) async {
+    try {
+      final response = await _dio.get(url, queryParameters: query);
+      _prefs.setString(url, jsonEncode(response.data));
+      return response;
+    } on DioException {
+      final cached = _prefs.getString(url);
+      if (cached != null) {
+        return Response(
+          data: jsonDecode(cached),
+          statusCode: 200,
+          requestOptions: RequestOptions(path: url),
+        );
+      }
+      rethrow;
+    }
+  }
+
+  Future<Response> getPaginated(
+    String url, {
+    Map<String, dynamic>? query,
+  }) async {
+    final fullQuery = {...?query};
+    final fullUrl = Uri.parse(
+      url,
+    ).replace(queryParameters: fullQuery).toString();
+
+    try {
+      final response = await _dio.get(url, queryParameters: fullQuery);
+      _prefs.setString(fullUrl, jsonEncode(response.data));
+      return response;
+    } on DioException {
+      final cached = _prefs.getString(fullUrl);
+      if (cached != null) {
+        return Response(
+          data: jsonDecode(cached),
+          statusCode: 200,
+          requestOptions: RequestOptions(path: fullUrl),
+        );
+      }
+      rethrow;
+    }
+  }
+}
